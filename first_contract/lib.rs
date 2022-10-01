5c59bed0e5d3a26c612353a31afa8810ab3f6c5d
Oleg Klimenko <37443840+klimenkoOleg@users.noreply.github.com>
2022-09-30 22:07:19 -0700

    Create lib.rs

---
+++ first_contract/lib.rs
@@ -0,0 +1,31 @@
+use ink_lang as ink;
+
+#[ink::contract]
+mod mycontract {
+
+	#[ink(storage)]
+	pub struct MyContract {
+		number: u32,
+	}
+
+	impl MyContract {
+		/// Constructor that initializes the `u32` value to the given `init_value`.
+		#[ink(constructor)]
+		pub fn new(init_value: u32) -> Self {
+			Self {
+				number: init_value,
+			}
+		}
+
+		/// Constructor that initializes the `u32` value to the `u32` default.
+		///
+		/// Constructors can delegate to other constructors.
+		#[ink(constructor)]
+		pub fn default() -> Self {
+			Self {
+				number: Default::default(),
+			}
+		}
+	/* --snip-- */
+	}
+}
